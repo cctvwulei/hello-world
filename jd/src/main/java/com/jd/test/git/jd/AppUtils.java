c484621d9fd453351bb697820ffed3eb8e710762
cctvwulei <1093341707@qq.com>
2017-05-26 14:43:00 +0800

    aaaaaaaa

---
+++ jd/src/main/java/com/jd/test/git/jd/AppUtils.java
@@ -0,0 +1,11 @@
+package com.jd.test.git.jd;
+
+/**
+ * Created by Administrator on 2017/5/26.
+ */
+public class AppUtils {
+
+    public static String getString(String str){
+        return  str + "aaaaaaaa";
+    }
+}
