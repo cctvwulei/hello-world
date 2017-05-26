01a0c20fe687a7a0247b310b395fc6c2aacc328c
cctvwulei <1093341707@qq.com>
2017-05-26 17:38:27 +0800

    12

--- jd/src/main/java/com/jd/test/git/jd/App.java
+++ jd/src/main/java/com/jd/test/git/jd/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World6" );
+        System.out.println( "Hello World7" );
         System.out.println( "Hello World5" );
     }
 }
